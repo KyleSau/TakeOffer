f5715cf16490df6ff661dac1c8be6ae0c56565f9
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2024-05-16 15:36:20 -0500

    s

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -16,9 +16,11 @@ const Navbar = () => {
     return (
         <header className="sticky top-0 left-0 z-50 w-full bg-white shadow-sm dark:bg-gray-950">
             <div className="container mx-auto flex items-center justify-between px-4 py-5 md:px-6">
-                <h1 className="text-2xl font-bold tracking-tight text-black sm:text-5xl md:text-6xl lg:text-7xl">
-                    TakeOffer
-                </h1>
+                <div>
+                    <h1 className="text-2xl font-bold tracking-tight text-black sm:text-5xl md:text-6xl lg:text-7xl">
+                        <Link href="#">TakeOffer</Link>
+                    </h1>
+                </div>
                 <button onClick={toggleMenu} className="md:hidden focus:outline-none z-50">
                     {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
                 </button>
